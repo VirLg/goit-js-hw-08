b017bc43c4a6e28ac90215148683a7f059fbb962
VirLg <108211942+VirLg@users.noreply.github.com>
2022-12-14 01:33:26 +0200

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,8 +3,54 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
+
+
+{/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */}
+
+const markupDiv = document.querySelector('.gallery');
+const markup = galleryItems.reduce((acc, { preview, original, description }) => acc+
+    `
+    <li>
+         <div class="gallery__item">
+           
+             <img
+                class="gallery__image"
+               src="${preview}"
+               data-source="large-image.jpg"
+               alt="${description}" width ="200px">
+             
+           
+         </div>
+       </li>
+    `
+    ,'')
+markupDiv.insertAdjacentHTML("beforeend",markup)
+
+markupDiv.addEventListener('click', onClick)
+
+function onClick(evn) { 
+console.log(evn.Target);
+}
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
 
 
 
-console.log(SimpleLightbox);
+console.log(markup);
